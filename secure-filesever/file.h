7bc145601015430e0485af79d9d61b12050031c4
paddyw2 <p.withams@gmail.com>
2017-11-08 17:07:51 -0700

    Complete refactoring and documentation of file.h

--- secure-filesever/file.h
+++ secure-filesever/file.h
@@ -1,17 +1,21 @@
 /*
- * opens a file in the current directoy by name
- * and returns its contents as an unsigned char *
+ * Opens a file and reads a chunk of size determined
+ * by the constants.h file, beginning at the position
+ * indicated by the offset parameter
+ * Returns the success status of the operation
  */
 int server::get_file_128(char filename[], char * contents, int offset)
 {
+    int chunk_size;
+    int last = 0;
+
     // check if there are 16bytes left in file
     int filesize = get_filesize(filename);
     if(filesize < 0)
         return -1;
+
+    // calculate the size of chunk to read
     int remaining = filesize - offset;
-    int chunk_size;
-    int last = 0;
-    // CRYPTCHANGE //
     if(remaining > DATA_SIZE) {
         chunk_size = DATA_SIZE;
     } else {
@@ -26,17 +30,31 @@ int server::get_file_128(char filename[], char * contents, int offset)
         printf("File opening failed\n");
         exit(EXIT_FAILURE);
     }
+    
+    // seek to the offset position in the file
     fseek(fptr, offset, SEEK_SET);
-    // use size to allocate memory to
-    // file buffer
+
+    // read file data into contents parameter
     int status = fread(contents, sizeof(char), chunk_size, fptr);
+    // set appropriate flags indicating
+    // the size of data read, and whether
+    // or not it is the end of the file
     contents[LENGTH_INDEX] = chunk_size;
     contents[LAST_INDEX] = last;
-    // close file
+
+    // close file and return
+    // success status
     fclose(fptr);
     return status;
 }
 
+/*
+ * Writes length amount of data from the contents location
+ * to a file of name filename
+ * Uses the total_written variable to determine whether to
+ * append or create a new file
+ * Returns the success status of the function
+ */
 int server::write_file(char filename[], char * contents, int length, int total_written)
 {
     int status;
@@ -53,7 +71,6 @@ int server::write_file(char filename[], char * contents, int length, int total_w
             exit(EXIT_FAILURE);
         }
     }
-    //fseek(fptr, offset, SEEK_SET);
     // use size to allocate memory to
     // file buffer
     status = fwrite(contents, sizeof(char), length, fptr);
@@ -62,6 +79,11 @@ int server::write_file(char filename[], char * contents, int length, int total_w
     return status;
 }
 
+/*
+ * Returns the filesize of the specified
+ * filename
+ * If an error occurs, returns -1
+ */
 int server::get_filesize(char filename[])
 {
     // create and open file
